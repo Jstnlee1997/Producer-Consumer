8ae96151645bd0b95addf21562e1f5cda6556434
Jstnlee1997 <jstnlee1997@gmail.com>
2021-11-29 16:24:11 +0000

    Read in argument line arguments

--- main.cc
+++ main.cc
@@ -8,10 +8,35 @@
 void *producer (void *id);
 void *consumer (void *id);
 
+struct Job {
+  int id;
+  int duration = rand() % 10 + 1;
+};
+
 int main (int argc, char **argv)
 {
 
   // TODO
+
+  // Initialise argument line variables
+  int queueSize, numberOfJobs, numberOfProducers, numberOfConsumers;
+  
+  // Ensure that there are four argument line arguments (5 including execution)
+  if (argc != 5) {
+    cerr << "There has to be 4 argument line arguments!\n"; 
+    return 0;
+  }
+
+  // Receive input of command line arguments and ensure that they are appropriate
+  if (check_arg(argv[1]) == -1 || check_arg(argv[2]) == -1 || check_arg(argv[3]) == -1 || check_arg(argv[4]) == -1) {
+    cerr << "Argument line arguments have to be integers!\n";
+    return 0;
+  } else {
+    queueSize = check_arg(argv[1]);
+    numberOfJobs = check_arg(argv[2]);
+    numberOfProducers = check_arg(argv[3]);
+    numberOfConsumers = check_arg(argv[4]);
+  }
 
   pthread_t producerid;
   int parameter = 5;
@@ -34,7 +59,7 @@ void *producer (void *parameter)
 
   cout << "Parameter = " << *param << endl;
 
-  sleep (5);
+  sleep (1);
 
   cout << "\nThat was a good sleep - thank you \n" << endl;
 
